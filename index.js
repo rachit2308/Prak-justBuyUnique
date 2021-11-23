580f215293ebb8dbb77e80bad0f8e220aef589d6
Priyansh Srivastava <priyansh.srivastava_cs19@gla.ac.in>
2021-11-23 22:30:59 +0530

    viewmore changes updation

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function myFunction()
 {
-    alert("Congrats! Your Awesome product is added to cart.")
+    alert("Congrats! Your Awesome product is added to cart.");
+}
+
+function mySecFunction()
+{
+    alert("Please, First Log in to your account!");
 }
